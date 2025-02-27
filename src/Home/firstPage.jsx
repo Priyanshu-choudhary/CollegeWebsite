289a51aec9bc62f53926b87b0fa59837d11cc674
Priyanshu-choudhary <broyadi23@gmail.com>
2025-02-27 16:09:52 +0530

    test WF

--- src/Home/firstPage.jsx
+++ src/Home/firstPage.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold text-blue-600">Welcome to Home Page</h1>
+      <h1 className="text-4xl font-bold text-blue-600">Welcome to clg Page</h1>
       <Button variant="contained" color="primary" className="mt-4">
         <Link to="/about" className="text-white">Go to About</Link>
       </Button>
